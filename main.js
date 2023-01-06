79ffd24fda1d3400f2cebcac5990a3adad6ac41e
Victor Paranhos <victor_paranhos@hotmail.com>
2023-01-06 16:31:02 -0300

    Up

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function route(event) {
+  event = event || window.event
+  event.preventDefault()
+  window.history.pushState({},"", event.target.href)
+  handle()
+}
+
+function handle(){
+  const { pathname }= window.location
+  const route = routes[pathname]
+  fetch(route)
+  .then(data => data.text())
+  .then(html => document.querySelector('#app').innerHTML = html)
+
+}
+
+const routes = {
+  "/": "/pages/home.html",
+  "/universo": "/pages/ouniverso.html",
+  "/exploracao": "/pages/exploracao.html",
+}
+
+handle()
