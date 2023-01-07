a9c4ec91cbf4e049aaeb0d822c33a94922b0a8a3
Victor Paranhos <victor_paranhos@hotmail.com>
2023-01-06 22:29:17 -0300

    UP

--- main.js
+++ main.js
@@ -6,9 +6,27 @@ function route(event) {
   handle()
 }
 
+const main = document.querySelector('main')
+
 function handle(){
-  const { pathname }= window.location
-  const route = routes[pathname] || routes["/"]
+  const { pathname } = window.location
+  const route = routes[pathname] || routes["/home"]
+
+  if(routes[pathname] == routes["/universo"]){
+   main.style.background = "url(./files/mountains-universe02.png"
+   main.style.backgroundSize = "cover"
+  }
+
+  if(routes[pathname] == routes["/exploracao"]){
+    main.style.background = "url(./files/mountains-universe-3.png)"
+    main.style.backgroundSize = "cover"
+  }
+
+  if(routes[pathname] == routes["/home"]){
+    main.style.background = "url(./files/mountains-universe-1.png)"
+    main.style.backgroundSize = "cover"
+  }
+
   fetch(route)
   .then(data => data.text())
   .then(html => document.querySelector('#app').innerHTML = html)
@@ -16,7 +34,7 @@ function handle(){
 }
 
 const routes = {
-  "/": "/pages/home.html",
+  "/home": "/pages/home.html",
   "/universo": "/pages/ouniverso.html",
   "/exploracao": "/pages/exploracao.html",
 }
